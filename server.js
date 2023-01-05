c18619375a926a6449a7334c1a74e61fb1a4d7cc
Nipendra Soni <rsoni2843@gmail.com>
2023-01-05 12:05:55 +0530

    json server for mock-4

---
+++ server.js
@@ -0,0 +1,24 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const port = 8080 || 3001;
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(jsonServer.bodyParser);
+server.use(
+  jsonServer.rewriter({
+    "/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(port, () => {
+  console.log("JSON Server is runningon port", port);
+});
+
+// Export the Server API
+module.exports = server;
